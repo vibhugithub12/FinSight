9e6646aa9f1a9dc50c303f3d14d96c58d2784f23
vibhugithub12 <vibhuchauhan37@gmail.com>
2025-12-21 12:24:36 +0530

    Contact page started

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,10 @@
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="flex flex-col">
+      <h2 className="text-2xl">You can contact me here :</h2>
+      <p className="text-lg">Gmail - vaibhavvchauhan37@gmail.com</p>
+    </div>
   )
 }
 
